da765346eab49f78987a7defb57cc980ceb216ea
Red_m0rdred <samrlipton@gmail.com>
2019-02-08 16:27:27 -0500

    Create Script.ps1

---
+++ Script.ps1
@@ -0,0 +1,16 @@
+$code = @"
+    [DllImport("user32.dll")]
+    public static extern bool BlockInput(bool fBlockIt);
+"@
+
+$userInput = Add-Type -MemberDefinition $code -Name UserInput -Namespace UserInput -PassThru
+
+function Disable-UserInput($seconds) {
+    $userInput::BlockInput($true)
+    Start-Sleep $seconds
+    $userInput::BlockInput($false)
+}
+
+Disable-UserInput -seconds 4 | Out-Null
+
+firefox
